ed08fa88bbb14e2a6fd635ecc39d3306b14d476f
lee5hx <lishengxi@mindasset.com>
2017-05-09 09:34:26 +0800

    同步TB库存,验证同步

--- src/main/java/com/myjo/ordercat/http/TaoBaoHttp.java
+++ src/main/java/com/myjo/ordercat/http/TaoBaoHttp.java
@@ -33,8 +33,8 @@ public class TaoBaoHttp {
     //private static final String SESSION_KEY = OrderCatConfig.getTaobaoApiSessionKey();
     //
 
-    //private static final Long CID = Long.parseLong("1282880675"); //麦巨自营
-    private static final Long CID = Long.parseLong("1316012271");//测试类目
+    private static final Long CID = Long.parseLong("1282880675"); //麦巨自营
+    //private static final Long CID = Long.parseLong("1316012271");//测试类目
 
 
     public TaoBaoHttp() {
